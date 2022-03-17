00393c336eb716b3febb3555857df527f73309e6
Ian Morgan <ian.j.morgan@gmail.com>
2022-03-17 16:35:30 +0000

    InBuiltTasks registrations now registers interface correctly

--- impl/src/main/kotlin/dreifa/app/tasks/inbuilt/InBuiltTasks.kt
+++ impl/src/main/kotlin/dreifa/app/tasks/inbuilt/InBuiltTasks.kt
@@ -2,23 +2,22 @@ package dreifa.app.tasks.inbuilt
 
 import dreifa.app.tasks.SimpleTaskRegistrations
 import dreifa.app.tasks.TaskRegistration
-import dreifa.app.tasks.inbuilt.fileBundle.FBQueryTaskImpl
-import dreifa.app.tasks.inbuilt.fileBundle.FBRetrieveTaskImpl
-import dreifa.app.tasks.inbuilt.fileBundle.FBStoreTaskImpl
+import dreifa.app.tasks.inbuilt.fileBundle.*
+import dreifa.app.tasks.inbuilt.providers.TPScanJarTask
 import dreifa.app.tasks.inbuilt.providers.TPScanJarTaskImpl
 
 class InBuiltTasks : SimpleTaskRegistrations(
     listOf(
         // FileBundle
-        TaskRegistration(FBQueryTaskImpl::class),
-        TaskRegistration(FBRetrieveTaskImpl::class),
-        TaskRegistration(FBStoreTaskImpl::class),
+        TaskRegistration(FBQueryTaskImpl::class, FBQueryTask::class),
+        TaskRegistration(FBRetrieveTaskImpl::class, FBRetrieveTask::class),
+        TaskRegistration(FBStoreTaskImpl::class, FBStoreTask::class),
 
         // Working with Providers
-        TaskRegistration(TPScanJarTaskImpl::class),
+        TaskRegistration(TPScanJarTaskImpl::class, TPScanJarTask::class),
 
         // Networking
-        TaskRegistration(DeterminePrivateIpAddressTaskImpl::class),
-        TaskRegistration(DeterminePublishedIpAddressTaskImpl::class)
+        TaskRegistration(DeterminePrivateIpAddressTaskImpl::class, DeterminePrivateIpAddressTask::class),
+        TaskRegistration(DeterminePublishedIpAddressTaskImpl::class, DeterminePublishedIpAddressTask::class)
     )
 )
